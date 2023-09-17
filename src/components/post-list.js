6212eea52e8da7d2ff512af00fec23ca5d657078
ampil <33726853+ampil@users.noreply.github.com>
2023-09-17 15:25:14 +0300

    hide scroll on authors
    + теперь нет скролла в Chromium, Edge, когда больше 1 автора
    + мелкие корректировки постов

--- src/components/post-list.js
+++ src/components/post-list.js
@@ -184,4 +184,15 @@ const TagListProp = styled.div`
   display: flex;
   text-overflow: ellipsis;
   white-space: nowrap;
+
+  ::-webkit-scrollbar {
+    display: none;
+  }
+  
+  /* Hide scrollbar for IE, Edge and Firefox */
+   {
+    -ms-overflow-style: none;  /* IE and Edge */
+    scrollbar-width: none;  /* Firefox */
+  } 
+
 `;
